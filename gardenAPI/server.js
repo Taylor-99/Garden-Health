4deafb575287b861c8c714d99dd119540cc33de9
User Taylor <elexus0201@gmail.com>
2024-05-25 21:52:41 -0400

    dependencies

--- gardenAPI/server.js
+++ gardenAPI/server.js
@@ -1,3 +1,14 @@
+
+// Dependencies
 const express = require('express');
+const mongoose = require("mongoose");
 
 const app = express();
+
+const PORT = process.env.PORT||3000
+
+
+// App Listen
+app.listen(PORT, ()=> {
+    console.log(`Listening to port ${PORT}`);
+  });  
