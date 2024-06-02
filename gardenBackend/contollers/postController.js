7c7a6c0fb47dd43030760c7c899d158d9b1cfbdb
User Taylor <elexus0201@gmail.com>
2024-06-02 00:05:33 -0400

    get all posts

--- gardenBackend/contollers/postController.js
+++ gardenBackend/contollers/postController.js
@@ -1,7 +1,24 @@
 
 require('dotenv').config();
 const router = require('express').Router();
-const jwt = require('jsonwebtoken');
+const db  = require('../models');
+
 const verifyToken = require('../middleware/VerifyJWT');
+
+// Show
+router.get('/', verifyToken, async (req, res) => {
+
+    try {
+
+        const posts = await db.CommunityPost.find()
+
+        res.send(posts)
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+
+});
 
 module.exports = router
