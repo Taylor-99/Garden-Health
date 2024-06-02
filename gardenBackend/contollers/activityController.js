85439bd827804dfdf97824ca222686da8ca38d58
User Taylor <elexus0201@gmail.com>
2024-06-01 22:36:41 -0400

    show Activity logs

--- gardenBackend/contollers/activityController.js
+++ gardenBackend/contollers/activityController.js
@@ -6,4 +6,20 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const verifyToken = require('../middleware/VerifyJWT');
 
+// Show
+router.get('/', verifyToken, async (req, res) => {
+
+    try {
+
+        const userActivity = await db.Activity.find({ user: req.user.userID})
+
+        res.send(userActivity)
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+
+});
+
 module.exports = router
